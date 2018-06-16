60baa086ffa852b757b763bdfa838e3eb3f43ac5
escarrion <eriksc@itikn.com>
2018-06-15 20:52:28 -0400

    adding assignment 2 for escarrion

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+##make a special matrix object we can pass to the cacheSolve function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(y){
+        x <<- y
+        inverse <<- NULL
+    }
+    
+    get <- function() x
+    
+    setinv <- function(solve) inverse <<- solve
+    getinv <- function() inverse
+    
+    list(set = set, get = get,
+         setinv = setinv, getinv = getinv, determinant = determinant )
 }
 
 
-## Write a short comment describing this function
+## Return the inverse of the object created by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinv()
+    if(!is.null(inverse)){
+        message('getting cached data')
+        return(inverse)
+    }
+    data <- x$get()
+    inverse <- solve(data,...)
+    x$setinv(inverse)
+    inverse
 }
